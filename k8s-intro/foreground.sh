d6d98134024b662fd49796ada3b607c2d8087b9f
calinah <calina1391@gmail.com>
2021-12-14 12:19:45 -0800

    rm foreground

--- k8s-intro/foreground.sh
+++ k8s-intro/foreground.sh
@@ -1,6 +1,6 @@
-#/bin/bash
+# #/bin/bash
 
-printf 'Hello this is your new playground.\n\n'
-printf 'Press on the arrow next to any command to execute that command.\n\n'
-printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
-printf 'Or type the commands you want to run as you would on your own terminal.\n\n'
+# printf 'Hello this is your new playground.\n\n'
+# printf 'Press on the arrow next to any command to execute that command.\n\n'
+# printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
+# printf 'Or type the commands you want to run as you would on your own terminal.\n\n'
